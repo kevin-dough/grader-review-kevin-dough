39d2c7de686069fc868cbea8f124bb301decc70a
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2023-03-01 13:38:30 -0800

    :D

--- grade.sh
+++ grade.sh
@@ -1,5 +1,7 @@
 set -e
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+# the junit commands are made for windows
 
 rm -rf student-submission
 git clone $1 student-submission
